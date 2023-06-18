843895aef5b2b8813bed41ddb5148b5fce6e40a8
killa739 <markeloffo173@yandex.ru>
2023-06-19 00:06:48 +0400

    add .env

--- bot.js
+++ bot.js
@@ -1,6 +1,5 @@
 const { Telegraf } = require('telegraf')
 
-const token = '5395908545:AAFj4hgz3sSS9Gk4ZQoRo5fQKSHKqIAPQ0g' 
-const bot = new Telegraf(token)
+const bot = new Telegraf (process.env.BOT_TOKEN)
 
 module.exports = bot
